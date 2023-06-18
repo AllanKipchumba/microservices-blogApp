6bea59ae59b39be8eaf0843ff026ae8158a6420b
allan <allankipchumba76@gmail.com>
2023-06-18 13:05:51 +0300

    emmiting events from posts and comments services to the event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyPasrser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyPasrser.json());
@@ -13,17 +14,32 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-
   comments.push({ id, content });
-
   commentsByPostId[req.params.id] = comments;
 
+  //emmit comment to event broker
+  await axios.post("http://localhost:4005/events", {
+    type: "commentCreated",
+    data: {
+      id: commentsByPostId,
+      content,
+      postID: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+//receive event from event broker
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => console.log("Listening on port 4001"));
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyPasrser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyPasrser.json());
@@ -13,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -22,7 +23,23 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  //emmit post to event broker
+  await axios.post("http://localhost:4005/events", {
+    type: "postCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+//receive event from event broker
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => console.log("Listening on port 4000"));
