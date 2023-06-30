876fd1a8058335460d0e237807b760fd53a90a5a
allan <allankipchumba76@gmail.com>
2023-06-30 08:22:24 +0300

    intergrated post and event bus services to communicate via kubernetes

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,9 +9,9 @@ app.post("/events", (req, res) => {
   const event = req.body;
 
   //emmit events to listeners
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  // axios.post("http://localhost:4001/events", event);
+  // axios.post("http://localhost:4002/events", event);
 
   res.send({ status: "OK" });
 });
